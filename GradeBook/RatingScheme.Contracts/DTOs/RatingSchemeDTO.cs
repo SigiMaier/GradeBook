a9542a0fe1df0cbb53ecf2fa3a4c9d02cf649c77
Siegfried Maier <siegfried.maier@cohu.com>
2019-02-18 16:49:50 +0100

    First Commit
    Added RatingScheme, corresponding Interface and Tests.
    Warnings need to be resolved and Last test in RatingScheme.Tests has to be finished

---
+++ GradeBook/RatingScheme.Contracts/DTOs/RatingSchemeDTO.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="RatingSchemeDTO.cs" company="Sigi Maier">
+// No copyright
+// </copyright>
+
+namespace RatingScheme.Contracts
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// DTO that contains all required RatingScheme data.
+    /// </summary>
+    public class RatingSchemeDTO
+    {
+        /// <summary>
+        /// Gets or sets the Number Of Problems for the Exam.
+        /// </summary>
+        public int NumberOfProblems { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Points Per Problem for the Exam.
+        /// </summary>
+        public Dictionary<int, int> PointsPerProblem { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Maximum Points of the Exam.
+        /// </summary>
+        public int MaximumPoints { get; set; }
+    }
+}
