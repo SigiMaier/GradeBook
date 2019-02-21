eb1aa61f7abf7f43fa054538646a186896b86549
SigiMaier <mail.siegfried.maier@gmail.com>
2019-02-21 17:35:47 +0100

    Added further Tests

--- GradeBook/Base.Test/FileHandling/XmlSerializerTest.cs
+++ GradeBook/Base.Test/FileHandling/XmlSerializerTest.cs
@@ -56,5 +56,74 @@ namespace Base.Tests.FileHandling
             // CleanUp
             File.Delete(fileName);
         }
+
+        [TestMethod]
+        public void XmlSerializeTestClassWithUsageOfXmlAttribute_TestClassExists_FileExists()
+        {
+            // Arrange
+            SimpleTestClassWithUsageOfXmlAttribute testClass = new SimpleTestClassWithUsageOfXmlAttribute()
+            {
+                HouseNumber = 4,
+                StreetName = "Edlinger Str.",
+                City = "Baierbach"
+            };
+
+            string fileName = this.applicationPath + "\\simpleTestClassWithXmlAttribute.xml";
+
+            // Act
+            XmlSerializer.Serialize(testClass, fileName);
+
+            // Assert
+            Assert.IsTrue(File.Exists(fileName), "The *.xml File was created");
+
+            // CleanUp
+            File.Delete(fileName);
+        }
+
+        [TestMethod]
+        public void XmlSerializeTestClassWithUsageOfXmlRoot_TestClassExists_FileExists()
+        {
+            // Arrange
+            SimpleTestClassWithUsageOfXmlRoot testClass = new SimpleTestClassWithUsageOfXmlRoot()
+            {
+                HouseNumber = 4,
+                StreetName = "Edlinger Str.",
+                City = "Baierbach"
+            };
+
+            string fileName = this.applicationPath + "\\simpleTestClassWithXmlAttribute.xml";
+
+            // Act
+            XmlSerializer.Serialize(testClass, fileName);
+
+            // Assert
+            Assert.IsTrue(File.Exists(fileName), "The *.xml File was created");
+
+            // CleanUp
+            File.Delete(fileName);
+        }
+
+        [TestMethod]
+        public void XmlSerializeSimpleTestClassDirectory_TestClassExists_FileExists()
+        {
+            // Arrange
+            SimpleTestClassDirectory testClassDirectory = new SimpleTestClassDirectory();
+            testClassDirectory.TestClasses.Add(new SimpleTestClassWithoutAttributes()
+            { HouseNumber = 4, StreetName = "Edlinger Str.", City = "Baierbarch" });
+            testClassDirectory.TestClasses.Add(new SimpleTestClassWithoutAttributes()
+            { HouseNumber = 5, StreetName = "Simssee Straße", City = "Stephanskirchen" });
+
+            string fileName = this.applicationPath + "\\simpleTestClassDirectory.xml";
+
+            // Act
+            XmlSerializer.Serialize(testClassDirectory, fileName);
+
+            // Assert
+            Assert.IsTrue(File.Exists(fileName));
+
+            // Cleanup
+            File.Delete(fileName);
+
+        }
     }
 }
